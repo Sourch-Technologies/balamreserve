1d2da4a8a6cb4d6152a604a1b2e75a2e5080cd7f
masoodimubashir <masudimubashir@gmail.com>
2023-11-13 15:50:26 +0530

    about

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -30,69 +30,174 @@
                         </div>
                         <div class="team_box">
                             <h6>
-                                Kevin MurnaneAlejandra Dovali’
+                                Alejandra Dovali'
+                                <br>
+                                Degital Director
+
 
                             </h6>
                             <a href="">
-                                http://www.bcl.ky/Coats
+                                https://www.linkedin.com/in/alejandra-dovali-6000a7231/
                             </a>
-                            <p>
-                                Coats Investments Intl.
-                            </p>
+
                         </div>
                         <div class="team_box">
-                            <h6>atthew AsinariPrimeSquare
-                                Bould ConsultingLTD
-                                Consulting Solutions
+                            <h6>
+                                SwabackPartners
+                                <br>
+                                DesignandLandscape
+                                Architecture
+
+
+                            </h6>
+                            <a href="">
+                                https://www.swaback.com
+                            </a>
+
+                        </div>
+                        <div class="team_box">
+                            <h6>
+                                CBREConsulting
+                                <br> Consulting Solutions
+
+                            </h6>
+                            <a href="">
                                 https://www.cbre.com
-                                Dr. Stefan Harzen
-                                Biologist
-                                linkedin.com/in/drstefanharzen
-                                EDSA
-                                Land Planning
-                                https://www.edsaplan.com
+                            </a>
+
+                        </div>
+                        <div class="team_box">
+                            <h6>
+                                Matthew Asinari
+                                <br>
                                 Branding
-                                http://www.habitat2art.com</h6>
+
+                            </h6>
+                            <a href="">
+                                http://www.habitat2art.com
+                            </a>
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                Prime Square
+
+                            </h6>
+                            <a href="">
+                                https://www.minettprimesquare.com
+                            </a>
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                NancyConrad NetZero
+                                <br>
+                                Institute
+
+
+                            </h6>
+                            <a href="">
+                                linkedin.com/in/nancy-conrad-7226324
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                Dr. Stefan Harzen
+                                <br>
+                                Biologist
+
+                            </h6>
+                            <a href="">
+                                linkedin.com/in/drstefanharzen
+                            </a>
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                HVS
+                                <br>
+                                Consultingand Services
+                                <br>
+                                Organization
+                                <br>
+                            </h6>
+                            <a href="">
+                                https://www.hvs.com
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                Professor Will Holder
+
+                            </h6>
+                            <a href="">
+                                Board of Director
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                Victor Barret
+                                <br>
+                                Professor Will HolderConsulting and Services
+                                <br>
+                                Organization
+                                <br>
+
+                            </h6>
+                            <a href="">
+                                linkedin.com/in/victor-barrett-559b7ba
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                EDSA
+                                <br>
+                                Land Planning
+
+                            </h6>
+                            <a href="">
+                                https://www.edsaplan.com
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                John Barnes
+
+
+                            </h6>
+                            <a href="">
+                                Board of Directors
+
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                PWC LuxembourgYoungEngineering
+
+                                <br>
+                                Green Bonds
+
+                            </h6>
+                            <a href="">
+                                https://www.pwc.lu/en
+                            </a>
+
                         </div>
                         <div class="team_box">
-                            <h6></h6>
+                            <h6>
+                                YoungEngineering
+                                <br>
+                                Consultancy
+                            </h6>
+                            <a href="">
+                                https://www.yecbelize.com
+                            </a>
+
                         </div>
-                        <div class="team_box">
-                            <h6></h6>
-                        </div>
-                        <div class="team_box">
-                            <h6></h6>
-                        </div>
-                        <div class="team_box">
-                            <h6></h6>
-                        </div>
+
 
                     </div>
 
